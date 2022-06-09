f43818dbc888792764f454fbc513e5ca3c4f8345
ThanosWang <thanoswang@163.com>
2022-06-09 12:26:23 -0500

    Test

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import sys
-assert len(sys.argv) != 1
-filenames = sys.argv[1]
-addedfiles = filenames.split(',')
-for i in addedfiles:
-    assert i.endswith('.txt')
-    f = open(i,'r')
-    F = f.read()
-    assert 1 == F.count('./n') + F.count('!/n') + F.count('?/n')
+if len(sys.argv) == 1:
+    break
+else:
+    filenames = sys.argv[1]
+    addedfiles = filenames.split(',')
+    for i in addedfiles:
+        assert i.endswith('.txt')
+        f = open(i,'r')
+        F = f.read()
+        assert 1 == F.count('./n') + F.count('!/n') + F.count('?/n')
