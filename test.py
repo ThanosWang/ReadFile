a5fbbcb2fdaa3f857d0e32859d0cd4496085935f
ThanosWang <thanoswang@163.com>
2022-06-15 14:45:59 -0500

    Improved version

--- test.py
+++ test.py
@@ -23,12 +23,12 @@ else:
 
     # Feedback if missing necessary files
     NeccessaryFiles = ['__init__.py', 'object_library.py', 'function_library.py', 'write_param_card.py', 'particles.py', 'parameters.py', 'vertices.py', 'couplings.py', 'coupling_orders.py']
-    MissingFiles = [i for i in NeccessaryFiles if not in ModelFiles]
+    MissingFiles = [i for i in NeccessaryFiles if i not in ModelFiles]
     if MissingFiles != []:
         print('Your model lacks these files:')
         print(MissingFiles)
         print('Sorry, your upload fails since your model is short of some necessary files.')
-        
+
     # Check required files for UFO Models
     assert os.path.exists('__init__.py')
 
