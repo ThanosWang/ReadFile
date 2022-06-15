32863d75dc726a7b659357632087ba1e5ad392b6
ThanosWang <thanoswang@163.com>
2022-06-15 14:21:16 -0500

    First Version testing Uploaded files

--- test.py
+++ test.py
@@ -3,15 +3,41 @@ import sys
 import os
 
 if len(sys.argv) == 1:
+    # If no file added, just finish the check
     sys.exit()
 else:
+    # Find names of added files
     filenames = sys.argv[1]
     addedfiles = filenames.split(',')
+    print('Your added Files are:')
+    print(addedfiles)    
+
+    # Find path of added files
     path = os.path.dirname(os.path.realpath(addedfiles[0]))
+
+    # Lead to the directory of added files
     os.chdir(path)
-    assert os.path.exists('model_description.txt')
-    for i in os.listdir():
-        assert i.endswith('.txt')
-        files = open(i,'r')
-        Files = files.read()
-        assert 1 == Files.count('.\n') + Files.count('!\n') + Files.count('?\n')
+
+    # Check required files for UFO Models
+    assert os.path.exists('__init__.py')
+
+    assert os.path.exists('object_library.py')
+
+    assert os.path.exists('function_library.py')
+
+    assert os.path.exists('write_param_card.py')
+    
+    assert os.path.exists('particles.py')
+
+    assert os.path.exists('parameters.py')
+    
+    assert os.path.exists('vertices.py')
+
+    assert os.path.exists('couplings.py')
+
+    assert os.path.exists('lorentz.py')
+    
+    assert os.path.exists('coupling_orders.py')
+
+    print('Your model contains neccessary files for UFO Model. Uploading succeeded. Thank you!')
+    
