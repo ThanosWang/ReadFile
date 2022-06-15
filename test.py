a72702520cf31d19a7d930063e548975efd27632
ThanosWang <thanoswang@163.com>
2022-06-15 14:42:34 -0500

    Improved version

--- test.py
+++ test.py
@@ -9,15 +9,26 @@ else:
     # Find names of added files
     filenames = sys.argv[1]
     addedfiles = filenames.split(',')
-    print('Your added Files are:')
-    print(addedfiles)    
-
+    
     # Find path of added files
     path = os.path.dirname(os.path.realpath(addedfiles[0]))
 
     # Lead to the directory of added files
     os.chdir(path)
+    
+    # Show all files in the model
+    ModelFiles = os.listdir()
+    print('Your model contains')
+    print(ModelFiles)
 
+    # Feedback if missing necessary files
+    NeccessaryFiles = ['__init__.py', 'object_library.py', 'function_library.py', 'write_param_card.py', 'particles.py', 'parameters.py', 'vertices.py', 'couplings.py', 'coupling_orders.py']
+    MissingFiles = [i for i in NeccessaryFiles if not in ModelFiles]
+    if MissingFiles != []:
+        print('Your model lacks these files:')
+        print(MissingFiles)
+        print('Sorry, your upload fails since your model is short of some necessary files.')
+        
     # Check required files for UFO Models
     assert os.path.exists('__init__.py')
 
@@ -39,5 +50,5 @@ else:
     
     assert os.path.exists('coupling_orders.py')
 
-    print('Your model contains neccessary files for UFO Model. Uploading succeeded. Thank you!')
+    print('Your model contains neccessary files for UFO Model. Upload succeeded. Thank you!')
     
